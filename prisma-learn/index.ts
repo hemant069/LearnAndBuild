6939d94cc0b647c1b48d3bdb91da2812f1535251
hemant069 <hemantprajapati20500@gmail.com>
2026-02-18 19:58:59 +0530

    writed first api

--- prisma-learn/index.ts
+++ prisma-learn/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { configDotenv
  } from "dotenv";
-
-
+import z from "zod/v4";
+import { prisma } from "./lib/prisma";
+import { error } from "node:console";
 
 const app=express();
 const PORT=8080
@@ -12,6 +13,50 @@ app.use(express.urlencoded({extended:true}));
 
 app.get('/',async(req,res)=>{
     res.send("Hello world")
+})
+
+
+const signupSchema=z.object({
+    name:z.string(),
+    email:z.string(),
+    password:z.string().max(6)
+
+})
+
+
+
+
+app.post('/api/signup',async(req,res)=>{
+
+    try {
+        
+        const parsed=signupSchema.safeParse(req.body);
+
+        if(!parsed.success){
+            res.status(404).json({error:parsed.error});
+        }
+
+        const {name,email,password}=parsed.data;
+
+        const existinguser=await prisma.User.findFirst({email});
+
+        if(existinguser){
+            res.status(409).json({error:"email is already taken"});
+
+        }
+
+        const user=await prisma.create({
+            data:{name,email,password}
+        })
+
+        res.status(201).json({data:"user created success"});
+        
+
+
+
+    } catch (error) {
+        
+    }
 })
 
 
