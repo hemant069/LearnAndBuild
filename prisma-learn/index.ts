78cb74d26e5b5c13b41a8599f9c84a8d5db6d766
hemant069 <hemantprajapati20500@gmail.com>
2026-02-19 22:37:19 +0530

    changes

--- prisma-learn/index.ts
+++ prisma-learn/index.ts
@@ -1,9 +1,6 @@
 import express from "express";
-import { configDotenv
- } from "dotenv";
 import z from "zod/v4";
 import { prisma } from "./lib/prisma";
-import { error } from "node:console";
 
 const app=express();
 const PORT=8080
@@ -12,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/',async(req,res)=>{
-    res.send("Hello world")
+  return   res.send("Hello world")
 })
 
 
@@ -33,23 +30,23 @@ app.post('/api/signup',async(req,res)=>{
         const parsed=signupSchema.safeParse(req.body);
 
         if(!parsed.success){
-            res.status(404).json({error:parsed.error});
+          return   res.status(404).json({error:parsed.error});
         }
 
         const {name,email,password}=parsed.data;
 
-        const existinguser=await prisma.User.findFirst({email});
+        const existinguser=await prisma.user.findFirst({where:{email}});
 
         if(existinguser){
-            res.status(409).json({error:"email is already taken"});
+           return  res.status(409).json({error:"email is already taken"});
 
         }
 
-        const user=await prisma.create({
+        const user=await prisma.user.create({
             data:{name,email,password}
         })
 
-        res.status(201).json({data:"user created success"});
+      return   res.status(201).json({data:"user created success"});
         
 
 
