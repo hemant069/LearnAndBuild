d78a861be2481847f52f44a6f5bef8cdd72c29bb
hemant069 <hemantprajapati20500@gmail.com>
2026-03-09 19:00:50 +0530

    added the user routers

--- social-media/src/routes/index.ts
+++ social-media/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authrouter from "./auth.routes";
+import  userRouter  from "./user.routes";
 
 
 
 const router=Router();
 
 router.use('/auth',authrouter);
+router.use('/user',userRouter)
 
 
 export default router;
